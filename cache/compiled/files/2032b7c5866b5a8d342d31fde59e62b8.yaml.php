8cb071e5de975103d809a06e8b4c19db6644cc7e
Geoff Sowrey <geoff@sowrey.org>
2021-09-26 21:01:26 -0600

    Project update (address missing files)

--- cache/compiled/files/2032b7c5866b5a8d342d31fde59e62b8.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/geoff/Sites/sowrey.grav/user/config/media.yaml',
-    'modified' => 1632146550,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/f1c8439b13d630eac45bc0fc25dfa4ce.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/geoff/Sites/sowrey.grav/user/config/security.yaml',
-    'modified' => 1632146081,
-    'data' => [
-        'salt' => 'wA0suqOQYJtI4R'
-    ]
-];
